a9522ebf3f5242a688009eb641fa81fa63b99127
Mauro Goyeneche <maurogoyeneche@gmail.com>
2020-07-06 22:00:38 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
 var inputSearchValue;
-
 // var products = [
-
-
 //     {
 //         id: 0,
 //         productName: "Play Station 5", 
@@ -20,7 +17,6 @@ var inputSearchValue;
 //         secondDescription:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium iste beatae sequi vero assumenda in. ",
 //         price: 16325,
 //         img: "assets/img/ps5_7.jpg"
-       
 //     },
 //     {
 //         id: 2, 
@@ -60,13 +56,11 @@ var inputSearchValue;
 //     }
 // ]
 var order = [];
-
 function addOrder(index){
     order.push(products[index]);
     localStorage.setItem('order', JSON.stringify(order));
     showOrder();
 }
-
 
 function showOrder(){
     order = (localStorage.getItem('order')) ? JSON.parse(localStorage.getItem('order')) : [];
@@ -75,66 +69,52 @@ function showOrder(){
     var total = 0;
     order.forEach(function(order, index){
         orderContainer.append(`
-                <li class="item">
+                <li class="item" id="000${index}">
                     <div class="row">
                         <div class="col-10"> ${order.productName}  U$s${order.price} </div>
-                        <div class="col-2"><button type="button" id="${index}" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>
+                        <div class="col-2"><button type="button" data-price="${order.price}" id="${index}" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>
                     </div>
                 </li>
-        `)
+        `);
         total = total + order.price;
         totalOrderPrice.empty();
         totalOrderPrice.append(`<p><strong>Total: USD ${total} </strong></p>`); 
     })
     //borrar  elementos por su id de la lista de orders.  event.target = this
     $('.close').click(function () {
-        let indice = $(this).attr('id')
-        $(this).parent().parent().parent().remove()
-        order.splice(indice,1)
-        localStorage.setItem('order', JSON.stringify(order));
         
+        //remueve un producto
+        let indice = $(this).attr('id')
+        var removeIndex = order.findIndex(x => x.indice === indice.indice);
+        order.splice(removeIndex, 1);
+
+        //calcula el precio
+        let price = $(this).data('price');
+        $(`#000${indice}`).remove()
+        localStorage.setItem('order', JSON.stringify(order));
+        total = total - price;
+        totalOrderPrice.empty();
+        totalOrderPrice.append(`<p><strong>Total: USD ${total} </strong></p>`); 
     })
 }
-
-$('#btn-eraser').click(function(){
+$('#btn-destroyCart').click(function(){
     orderContainer.empty();
     order = [];
     totalOrderPrice.empty();
     localStorage.clear()
 })
-
-
-
+// Funcion que llama a renderProducts.js para inyectar el html de productos y el modal
 function showProducts(products) {
     products.forEach(function(product, index){
-        productsContainer.append(`
-                    <div id="hackerDiv" class="col-md-4 ;">
-                        <div id="card-product" class="card m-1 ">
-                            <img src="${product.img}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">${product.productName}</h5>
-                                <p class="card-text">${product.firstDescription}</p>
-                                <p class="price"><span class="dolar-price">USD <span class="dolar-price">${product.price}</span> </span> </p>
-                                <p id="card-txt" class="card-text">
-                                <!-- Button trigger modal -->
-                                    <button id="btn" type="button" class="btn btn-info btn-block btn-sm" data-toggle="modal" data-target="#modalDescription">+ Info</button>
-                                <!-- Button Add to Cart -->
-                                    <button id="add-to-cart" type="button" class="btn btn-dark btn-block btn-sm add-to-cart" data-id="${index}">Agregar al Carrito +</button>
-                                </p>
-                            </div>
-                        </div>
-                    </div>  
-        `)  
-    
-    
+        productsContainer.append(renderProducts(product, index))  
     })
     var btnAddToCart = $('.add-to-cart');
     btnAddToCart.click(function(event){
         var indexSelection = $(event.target).data("id");
         addOrder(indexSelection);
     })
-    
 }
+
 //obtener valor deL buscador
 function getInputSearchValue() {
     var inputSearchValue = inputSearch.val();
@@ -147,10 +127,7 @@ function searchKeyValue(key) {
     var searchKey = $('#search-key');
     searchKey.html(key);
 }
-
-
 $(document).ready(() => {
-
         $.ajax({
             method: 'GET',
             url: "assets/js/data.json",
@@ -158,21 +135,16 @@ $(document).ready(() => {
         }).done( function(products){
             // recibe el resultado y lo muestra en un elemento p
             showProducts(products);
-
             }).fail( function(){
             console.log('no ajax');
         })
-      
-
     titleModal = $('.title-modal');
     productsContainer = $('#products-container');
     orderContainer = $('#order-container');
     // showProducts();
     totalOrderPrice = $('#total-order-price');
     showOrder();
-
     inputSearch = $('#input-search');
-
     formSearchValue = $('#form-submit');
     formSearchValue.on('submit', function(event){
         event.preventDefault();
@@ -184,40 +156,20 @@ $(document).ready(() => {
           $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
         });
       });
-
-
- 
 //  showModal(); 
-
-
-
-   
-
-   
-
 })
-
-
-
-// Mostrar mensaje de Ã©xito:
-
+// Mostrar mensaje de Ã©️xito:
 function mostrarOK(texto) {
-
     // Por las dudas, cerrar alert previo:
     $(".alert").alert('close');
-
     var html_alert = "";
     html_alert += '<div class="alert alert-success alert-dismissible" role="alert">';
     html_alert += '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">Ã—</span></button>';
     html_alert += texto;
     html_alert += '</div>';
-
     // Mostrar alert luego del #content:
     $("#alert-text").after(html_alert);
 }
-
-
-
 ///////
 // function showOrder(){
 //     order = (localStorage.getItem('order')) ? JSON.parse(localStorage.getItem('order')) : [];
@@ -242,5 +194,3 @@ function mostrarOK(texto) {
 //         localStorage.setItem('order', JSON.stringify(order));
 //     })
 // }
-
-
