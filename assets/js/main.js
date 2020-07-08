e3b079080563a21f6ca64e754c28d3beb947634d
Mauro Goyeneche <maurogoyeneche@gmail.com>
2020-07-08 12:06:47 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,5 @@
 var inputSearchValue;
-// var products = [
-//     {
-//         id: 0,
-//         productName: "Play Station 5", 
-//         model: "OVNI", 
-//         firstDescription: "Tecnología Anti Gravity - VR",
-//         secondDescription:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium iste beatae sequi vero assumenda in. ", 
-//         price: 20325,
-//         img: "assets/img/ps5-lanzamiento.jpg"
-//     },
-//     {
-//         id: 1, 
-//         productName: "Play Station 5", 
-//         model: "SNOW", 
-//         firstDescription: "Tecnología  White Anti Gravity - VR", 
-//         secondDescription:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium iste beatae sequi vero assumenda in. ",
-//         price: 16325,
-//         img: "assets/img/ps5_7.jpg"
-//     },
-//     {
-//         id: 2, 
-//         productName: "Play Station 5", 
-//         model: "SPACE", 
-//         firstDescription: "Tecnología Responsive Anti Gravity - VR", 
-//         secondDescription:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium iste beatae sequi vero assumenda in. ",
-//         price: 26325,
-//         img: "assets/img/ps5_3.jpg"
-//     },
-//     {
-//         id: 3, 
-//         productName: "Play Station 5", 
-//         model: "CLASSIC", 
-//         firstDescription: "camera VR integrada", 
-//         secondDescription:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium iste beatae sequi vero assumenda in. ",
-//         price: 30225,
-//         img: "assets/img/ps5_8.jpg"
-//     },
-//     {
-//         id: 4, 
-//         productName: "JoyStick PS5", 
-//         model: "Touch", 
-//         firstDescription: "Retina Touch pad integrada - VR", 
-//         secondDescription:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium iste beatae sequi vero assumenda in. ",
-//         price: 26025,
-//         img: "assets/img/joystick_ps5.jpg"
-//     },
-//     {
-//         id: 5, 
-//         productName: "Play Station 5", 
-//         model: "SQUARE", 
-//         firstDescription: "Unbreakable - VR", 
-//         secondDescription:  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium iste beatae sequi vero assumenda in. ",
-//         price: 22325,
-//         img: "assets/img/ps5_6.jpg"
-//     }
-// ]
+
 var order = [];
 function addOrder(index){
     order.push(products[index]);
@@ -62,7 +7,7 @@ function addOrder(index){
     showOrder();
     mostrarOK("Se agregó el item al carrito");
 }
-
+//renderiza la Orden en el carrito
 function showOrder(){
     order = (localStorage.getItem('order')) ? JSON.parse(localStorage.getItem('order')) : [];
     totalOrderPrice.show('slow');
@@ -98,6 +43,7 @@ function showOrder(){
         totalOrderPrice.append(`<p><strong>Total: USD ${total} </strong></p>`); 
     })
 }
+//vacia el carrito y limpia el localstorage
 $('#btn-destroyCart').click(function(){
     orderContainer.empty();
     order = [];
@@ -109,6 +55,7 @@ function showProducts(products) {
     products.forEach(function(product, index){
         productsContainer.append(renderProducts(product, index))  
     })
+    //evento que agrega los items segun id  al carrito
     var btnAddToCart = $('.add-to-cart');
     btnAddToCart.click(function(event){
         var indexSelection = $(event.target).data("id");
@@ -129,6 +76,7 @@ function searchKeyValue(key) {
     var searchKey = $('#search-key');
     searchKey.html(key);
 }
+//inicio del documento
 $(document).ready(() => {
         $.ajax({
             method: 'GET',
@@ -140,25 +88,27 @@ $(document).ready(() => {
             }).fail( function(){
             console.log('no ajax');
         })
-    titleModal = $('.title-modal');
+
     productsContainer = $('#products-container');
     orderContainer = $('#order-container');
-    // showProducts();
     totalOrderPrice = $('#total-order-price');
     showOrder();
+
     inputSearch = $('#input-search');
+    //evito que el form me haga submit por defecto
     formSearchValue = $('#form-submit');
     formSearchValue.on('submit', function(event){
         event.preventDefault();
-        getInputSearchValue();
+    getInputSearchValue();
     })
+    //filtro
     $("#input-search").on("keyup", function() {
         var value = $(this).val().toLowerCase();
         productsContainer.filter(function() {
           $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
         });
       });
-//  showModal(); 
+
 })
 // Mostrar mensaje de Ã©️xito:
 function mostrarOK(texto) {
@@ -175,27 +125,3 @@ function mostrarOK(texto) {
     // Mostrar alert luego del #content:
     $(".alert-text").after(html_alert);
 }
-///////
-// function showOrder(){
-//     order = (localStorage.getItem('order')) ? JSON.parse(localStorage.getItem('order')) : [];
-//     orderContainer.empty();
-//     order.forEach(function(order, index){
-//         orderContainer.append(`
-//                 <li class="item">
-//                     <div class="row">
-//                         <div class="col-10"> ${order.productName}  U$s${order.price} </div>
-//                         <div class="col-2"><button type="button" id="${index}" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>
-//                     </div>
-//                 </li>
-//         `)
-//         total = total + order.price;
-//         totalOrderPrice.empty();
-//         totalOrderPrice.append(`<p><strong>Total: USD ${total} </strong></p>`); 
-//     })
-//     $('.close').click(function () {
-//         let indice = $(this).attr('id')
-//         $(this).parent().parent().parent().remove()
-//         order.splice(indice,1)
-//         localStorage.setItem('order', JSON.stringify(order));
-//     })
-// }
